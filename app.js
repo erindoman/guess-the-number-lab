4d1674f9365350f0227f933e9f45b53d7afea051
erindoman <erin.doman@gmail.com>
2020-11-09 09:09:24 -0600

    updated guess the number lab with notes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const game = {
+    //initialize state
     title: 'Guess the Number!',
     biggestNum: 100,
     smallestNum: 1,
@@ -6,36 +7,31 @@ const game = {
     prevGuesses: [],
     play: function() {
         this.secretNum = Math.floor(Math.random() * 
-            (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-        let guess = NaN;
-        while(guess !== this.secretNum) {
+            (this.biggestNum - this.smallestNum + 1)) + this.smallestNum; //random number 1-100
+        let guess = NaN; 
+        while(guess !== this.secretNum) { //this runs the game
             guess = this.getGuess();
             this.prevGuesses.push(guess);
             this.render(guess); 
             if (guess === this.secretNum) return;
         }
     },
-    getGuess: function() {
+    getGuess: function() { //this class gets the guess from the user
         let guess = NaN;
         while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
-          guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `));
+            guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `));
           
     // doesn't have a radix, so it is assumed to be 10 (8 if it starts with a 0)
 
         }
-        return guess;    
+        return guess;    //logs user's guess
     },
-    render: function(guess) {
+    render: function(guess) { //this class shows the message 
        let msg = (guess === this.secretNum) ?
-            `Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`
-        :
-            `
-            Your guess is too ${guess > this.secretNum ? 'high' : 'low'}
-            Previous guesses: ${this.prevGuesses.join(',')}
-            `
-        ;
+            `Congrats! You guessed the number in ${this.prevGuesses.length} guesses!` : `Your guess is too ${guess > this.secretNum ? 'high' : 'low'}
+            Previous guesses: ${this.prevGuesses.join(',')}`;
         alert(msg);
     }
 };
 
-console.log(game.play(2));
+game.play();
