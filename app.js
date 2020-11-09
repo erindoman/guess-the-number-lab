9bc0152d8fad23e989fe9ae3ecce84021faddf99
erindoman <erin.doman@gmail.com>
2020-11-08 18:31:32 -0600

    updated guess the number lab, not working

--- app.js
+++ app.js
@@ -4,19 +4,38 @@ const game = {
     smallestNum: 1,
     secretNum: null,
     prevGuesses: [],
+    play: function() {
+        this.secretNum = Math.floor(Math.random() * 
+            (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+        let guess = NaN;
+        while(guess !== this.secretNum) {
+            guess = this.getGuess();
+            this.prevGuesses.push(guess);
+            this.render(guess); 
+            if (guess === this.secretNum) return;
+        }
+    },
     getGuess: function() {
         let guess = NaN;
         while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
-          guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`, 10));  // doesn't have a radix, so it is assumed to be 10 (8 if it starts with a 0)
+          guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `));
+          
+    // doesn't have a radix, so it is assumed to be 10 (8 if it starts with a 0)
+
         }
         return guess;    
     },
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+    render: function(guess) {
+       let msg = (guess === this.secretNum) ?
+            `Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`
+        :
+            `
+            Your guess is too ${guess > this.secretNum ? 'high' : 'low'}
+            Previous guesses: ${this.prevGuesses.join(',')}
+            `
+        ;
+        alert(msg);
     }
-  };
+};
 
-//   console.log(game.getGuess())
-
-
+console.log(game.play(2));
